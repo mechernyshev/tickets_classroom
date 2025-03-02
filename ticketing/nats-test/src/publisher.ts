0f95c25c62d1a695dd92a8f6f3952d3bbded6e1c
mechernyshev <m.tchernyshev@gmail.com>
2025-03-02 12:40:45 +0200

    Refactor event publishing with base publisher class.
    Introduce an abstract `Publisher` class to standardize event publishing. Refactor `TicketCreatedPublisher` to extend the new base class, improving reusability and reducing redundancy. Update the publisher logic to leverage the new implementation for cleaner and more maintainable code.

--- ticketing/nats-test/src/publisher.ts
+++ ticketing/nats-test/src/publisher.ts
@@ -1,19 +1,23 @@
 import nats from 'node-nats-streaming';
+import {TicketCreatedPublisher} from "./events/ticket-created-publisher";
 
 const stan = nats.connect('ticketing', 'abc', {
     url: 'nats://localhost:4222'
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
     console.log('Publisher connected to NATS');
 
-    const data = JSON.stringify({
-        id: 1,
-        title: 'concert',
-        price: 10
-    });
+    const publisher = new TicketCreatedPublisher(stan);
+    try {
+        await publisher.publish({
+            id: '123',
+            title: 'concert',
+            price: 20,
+        });
+    } catch (err) {
+        console.error(err);
+    }
 
-    stan.publish('ticket:created', data, () => {
-        console.log('event published');
-    });
+
 })
