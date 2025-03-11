55f5a2d87b442bc653c708d3ce9434ed6e8374d2
mechernyshev <m.tchernyshev@gmail.com>
2025-03-11 15:32:54 +0200

    Add ticket creation and message acknowledgment tests
    Implemented unit tests to verify ticket creation and persistence in the database upon processing a TicketCreatedEvent. Also added tests to ensure the acknowledgment message is properly called after event handling.

--- ticketing/orders/src/events/listeners/__test__/ticket-created-listener.test.ts
+++ ticketing/orders/src/events/listeners/__test__/ticket-created-listener.test.ts
@@ -1,5 +1,51 @@
+import {TicketCreatedListener} from "../ticket-created-listener";
+import {TicketCreatedEvent} from "@mcgittix/common";
+import {natsWrapper} from "../../../nats-wrapper";
+import mongoose from "mongoose";
+import {Message} from "node-nats-streaming";
+import {Ticket} from "../../../models/ticket";
+
+const setup = async () => {
+    const listener = new TicketCreatedListener(natsWrapper.client);
+
+    const data: TicketCreatedEvent['data'] = {
+        version: 0,
+        id: new mongoose.Types.ObjectId().toHexString(),
+        title: 'concert',
+        price: 20,
+        userId: new mongoose.Types.ObjectId().toHexString(),
+    };
 
 
-it('creates and saves a ticket', async () => {})
+    // @ts-ignore
+    const msg: Message[] = {
+        // @ts-ignore
+        ack: jest.fn(),
+    }
 
-it('acks the message', async () => {})
+    return {listener, data, msg};
+}
+
+it('creates and saves a ticket', async () => {
+    const { listener,data,msg } = await setup();
+
+    // @ts-ignore
+    await listener.onMessage(data, msg);
+
+    const ticket = await Ticket.findById(data.id);
+
+    expect(ticket).toBeDefined();
+    expect(ticket!.title).toEqual(data.title);
+    expect(ticket!.price).toEqual(data.price);
+})
+
+it('acks the message', async () => {
+    const { listener,data,msg } = await setup();
+
+    // @ts-ignore
+    await listener.onMessage(data, msg);
+
+    // @ts-ignore
+    expect(msg.ack).toHaveBeenCalled();
+
+})
