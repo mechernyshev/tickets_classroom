f7f41ab1a47893c23bfbbc010f708ff454ffa94c
mechernyshev <m.tchernyshev@gmail.com>
2025-03-18 10:20:04 +0200

    Add Stripe integration for payment processing
    Integrated Stripe to handle payment workflows with a new configuration for the API key in Kubernetes. Added the Stripe library, initialized it in the project, and implemented charge creation for orders in the payments service.

--- ticketing/payments/src/routes/new.ts
+++ ticketing/payments/src/routes/new.ts
@@ -7,6 +7,7 @@ import {
     NotFoundError, NotAuthorizedError, OrderStatus,
 } from "@mcgittix/common";
 import {Order} from "../models/order";
+import stripe from "stripe";
 
 
 const router = express.Router();
@@ -44,6 +45,12 @@ router.post('/api/payments',
     if (order.status === OrderStatus.Cancelled) {
         throw new BadRequestError('Cannot pay for cancelled order');
     }
+
+    await stripe.charges.create({
+        currency: 'usd',
+        amount: order.price * 100,
+        source: token,
+    })
 
     res.send({success: true});
 })
