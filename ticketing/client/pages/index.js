4f672171bf0180025b66c79e304e3491b721f084
mechernyshev <m.tchernyshev@gmail.com>
2025-03-19 17:20:50 +0200

    Add ticket listing and creation functionality
    Enhanced the landing page to display a list of available tickets. Introduced a new page for creating tickets with appropriate form validation and submission logic. Updated the app layout to include consistent styling across pages.

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,17 +1,40 @@
-import buildClient from '../api/build-client';
+const LandingPage = ({ currentUser, tickets }) => {
+    console.log(tickets);
 
-const LandingPage = ({ currentUser }) => {
-    // console.log(currentUser);
-    // axios.get('/api/users/currentuser');
+    const ticketList = tickets.map(ticket => {
+        return (
+            <tr key={ticket.id}>
+                <td>{ticket.title}</td>
+                <td>{ticket.price}</td>
+            </tr>
+        )
+    })
 
-    return currentUser ? <h1>you are signed in!</h1> : <h1>You are not signed in</h1>;
+    return (
+        <div>
+            <h1>Tickets</h1>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {ticketList}
+                </tbody>
+            </table>
+        </div>
+    )
+
+
+
 };
 
-LandingPage.getInitialProps = async context => {
-    const client = buildClient(context);
-    const { data } = await client.get('/api/users/currentuser');
+LandingPage.getInitialProps = async (context, client, currentUser ) => {
+    const {data} = await client.get('/api/tickets');
 
-    return data;
+    return {tickets: data}
 };
 
 export default LandingPage;
